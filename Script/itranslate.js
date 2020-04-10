105bf69d43dad7d06d6507bbb3c8ac5432a78ff6
Eric <Alex0510@users.noreply.github.com>
2020-04-10 20:14:22 +0800

    itranslate

--- Script/itranslate.js
+++ Script/itranslate.js
@@ -4,5 +4,5 @@ https://ssl-api.itranslateapp.com/accounts/v4/subscriptions/verify/ios
 */
 
 var obj = JSON.parse($response.body);
-obj = {"licenses":[{"bundle_id":"com.outerspaceapps.itranslate","expires_date_ms":1587114775000,"is_trial_period":true,"original_transaction_id":"160000714859958","product_id":"com.itranslate.pro.monthly","transaction_id":"160000714859958"},{"bundle_id":"com.outerspaceapps.itranslate","expires_date_ms":3587114775000,"is_trial_period":true,"original_transaction_id":"160000714859958","product_id":"com.itranslate.pro.monthly","transaction_id":"160000714859958"}]}
+obj = {"licenses":[{"bundle_id":"com.outerspaceapps.itranslate","expires_date_ms":4587114775000,"is_trial_period":true,"original_transaction_id":"160000714859958","product_id":"com.itranslate.pro.monthly","transaction_id":"160000714859958"},{"bundle_id":"com.outerspaceapps.itranslate","expires_date_ms":4587114775000,"is_trial_period":true,"original_transaction_id":"160000714859958","product_id":"com.itranslate.pro.monthly","transaction_id":"160000714859958"}]}
 $done({body: JSON.stringify(obj)});
