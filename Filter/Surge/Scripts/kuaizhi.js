e76694f3298feae88dd97313e1d48274de354058
fangpidedongsun <55946288+fangpidedongsun@users.noreply.github.com>
2020-05-07 09:40:41 +0800

    Update kuaizhi.js

--- Filter/Surge/Scripts/kuaizhi.js
+++ Filter/Surge/Scripts/kuaizhi.js
@@ -6,11 +6,11 @@ hostname：kz.sync163.com
 
 
 let obj = JSON.parse($response.body);
-obj = {
+obj.data={
         "experiencer":1,
-        "vip_expired":"false",
+        "vip_expired":false,
         "vip":1,
         "vip_expired_at":2361-12-12,
-        "vip_is_gift":"true"
+        "vip_is_gift":true
       }
 $done({body: JSON.stringify(obj)});
