b626b704fafba9cbf7d5ae948ecfc1b9dcab4cbb
fangpidedongsun <55946288+fangpidedongsun@users.noreply.github.com>
2020-05-07 09:23:02 +0800

    UP

---
+++ Filter/Surge/Scripts/kuaizhi.js
@@ -0,0 +1,16 @@
+/*
+快知 解锁VIP
+https:\/\/kz.sync163.com\/api_v2\/user\/info
+hostname：kz.sync163.com
+*/
+
+
+var obj = JSON.parse($response.body);
+obj = {
+        "experiencer":1",
+        "vip_expired":false",
+        "vip":1",
+        "vip_expired_at":"2361-12-12",
+        "vip_is_gift":true"
+      }
+$done({body: JSON.stringify(obj)});
