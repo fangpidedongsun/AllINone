9205b9dae785bed8b54e32d47043bdee691f3bb6
fangpidedongsun <55946288+fangpidedongsun@users.noreply.github.com>
2020-12-16 09:05:24 +0800

    1

--- Filter/Surge/Sgmodule/CheckIn/10010.js
+++ Filter/Surge/Sgmodule/CheckIn/10010.js
@@ -7,7 +7,7 @@ m.client.10010.com
 联通_获取cookie = type=http-request,pattern=^https?:\/\/m\.client\.10010\.com\/dailylottery\/static\/(integral|doubleball)\/firstpage,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/10010/unicom_checkin.js,
 联通_签到与抽奖 = script-path=https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/10010/unicom_checkin.js,type=cron,cronexp=10 0 * * *
 */
-const getLotteryCookieRegex = /^https?:\/\/m\.client\.10010\.com\/dailylottery\/static\/(integral|doubleball)\/firstpage/;
+const getLotteryCookieRegex = /^https?:\/\/m\.client\.10010\.com\/dailylottery\/static\/(integral|doubleball)\/firstpage?/;
 const unicomCookieKey = 'unicom_user_cookie';
 const mobileKey = 'unicom_mobile'
 const encryptMobileKey = 'unicom_encrypt_mobile'
