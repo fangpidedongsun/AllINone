b7e67af31e3f3cb776cf645e1cac68b0d1c8f13c
fangpidedongsun <55946288+fangpidedongsun@users.noreply.github.com>
2021-01-07 11:11:25 +0800

    Update hongenshaungyu.js

--- Filter/Surge/Scripts/hongenshaungyu.js
+++ Filter/Surge/Scripts/hongenshaungyu.js
@@ -1,5 +1,17 @@
 
+/*
+[rewrite_local]
+# 洪恩(双语绘本、数学、儿童英语)
+^https:\/\/bookapi.ihuman.com:443\/v3\/get_book_info
+[mitm]
+bookapi.ihuman.com
+
 var obj = JSON.parse($response.body);
 
 obj = {"is_vip": 0}
-$done({body: JSON.stringify(obj)});
+*/
+
+
+body = $response.body.replace(/"is_vip":\{[^}]+\}/g /,"is_vip":0);
+
+$done({body});
