31219fbd610496e1d5d812eae33efc3294858294
fangpidedongsun <55946288+fangpidedongsun@users.noreply.github.com>
2021-01-08 12:29:12 +0800

    1

--- Filter/Surge/Sgmodule/广告屏蔽/yijiejiayou.js
+++ Filter/Surge/Sgmodule/广告屏蔽/yijiejiayou.js
@@ -1,11 +1,14 @@
 
 /*
 https://bjapi.ejoy.sinopec.com/api/advertisement-service/start-module/app/flashScreen
-*/
-let obj=JSON.parse($response.body);
-
+obj.resource=0;
 obj.countdown=0;
 obj.url="";
 obj.status=false;
+*/
+let obj=JSON.parse($response.body);
+obj['data']['resource'] = null;
+obj['data']['countdown'] = 0;
+obj['data']['url'] = null;
 
 $done({body: JSON.stringify(obj)})
