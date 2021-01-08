e11662da555570ab0125f2dca3e2b6b525883c21
fangpidedongsun <55946288+fangpidedongsun@users.noreply.github.com>
2021-01-08 10:51:53 +0800

    1

--- Filter/Surge/Sgmodule/广告屏蔽/yijie2.js
+++ Filter/Surge/Sgmodule/广告屏蔽/yijie2.js
@@ -1,5 +1,6 @@
 
 /*
+删除 
 https://bjapi.ejoy.sinopec.com/api/advertisement-service/advertisement/hotspot/app-list
 */
 let obj=JSON.parse($response.body);
@@ -8,5 +9,6 @@ obj.closeChannel=1;
 obj.closeChannelConfiguration="";
 obj.picture="";
 obj.pictureConfiguration="";
+
 
 $done({body: JSON.stringify(obj)})
